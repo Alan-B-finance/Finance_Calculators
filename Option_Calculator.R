beb9ccce28efd4a72f8e7208b2224fb4b1b0a0b4
Alan <37475891+Alan-B-finance@users.noreply.github.com>
2022-11-07 23:45:14 +0100

    Extending classes to Binomial Trees

--- Option_Calculator.R
+++ Option_Calculator.R
@@ -8,15 +8,17 @@ Option <- setClass(
     r = "numeric", #Risk Free Rate during the live of the Option
     cD = "numeric", #Context Date in yyyymmdd format
     mD = "numeric", #Maturity Date in yyyymmdd format
-    N = "numeric", #Number of steps
-    pu = "numeric", #Probability of up move
-    pd = "numeric", #Probability of down move
     div = "numeric", #dividend yield
     flag = "character", #c for call, p for put
     flavor = "character", #e for European, a for American
     
-    years = "numeric" #time to maturity in years
+    Years = "numeric", #time to maturity in years
+    DiscountFactorPerTimeStep = "numeric" #discouning factor used for drift
     ),
+  
+  prototype = list(
+    div = 0
+  ),
   
   validity = function(object){
     if(object@S0 < 0){
@@ -27,31 +29,70 @@ Option <- setClass(
       return("Error: Risk free rate can't be lower than 0")
     } else if(nchar(object@cD) != 8 | nchar(object@mD) != 8){
       return("Error: Please provide dates in the yyyymmdd format")
-    } else if(object@N%%1 != 0 | object@N < 1){
-      return("Error: Number of steps needs to be a natural number")
-    } else if(length(object@pu) != 0 & length(object@pd) != 0){
-      if(object@pu + object@pd != 1){
-        return("Error: The sum of probabilities needs to be 1")
-      } else if(object@pu < 0 | object@pd < 0){
-        return("Error: You can't have negtive probabilities")
-      } 
     } else if(!(object@flag %in% c("c", "p"))){
       return("Error: The flag can either be c (call) or p (put)")
     } else if(!(object@flavor %in% c("e", "a"))){
       return("Error: The flag can either be e (european) or a (american)")
     }
-  }
-)
-
-setGeneric(name="CalculateTime", def=function(optionName){
-  standardGeneric("CalculateTime")
-  }
+  },
+  
+  contains=c("VIRTUAL")
 )
 
 setMethod("initialize", "Option",
           function(.Object, ...) {
             .Object <- callNextMethod(.Object, ...)
             validObject(.Object)
-            .Object@years <- as.numeric(as.Date(as.character(.Object@mD), format("%Y%m%d")) - as.Date(as.character(.Object@cD), format("%Y%m%d")))/365
+            .Object@Years <- as.numeric(as.Date(as.character(.Object@mD), format("%Y%m%d")) - as.Date(as.character(.Object@cD), format("%Y%m%d")))/365
+            .Object@YearsPerTimesstep <- .Object@Years / .Object@N
+            .Object@DiscountFactorPerTimeStep <- exp(-(.Object@r - .Object@div)*.Object@YearsPerTimesstep)
             return(.Object)
-          })
+          }
+)
+
+TreeEuropeanOption <- setClass(
+  
+  "TreeEuropeanOption",
+  
+  slots = c(
+    N = "numeric", #Number of steps
+    
+    YearsPerTimesstep = "numeric" #duration of a single time step,in years
+  ),
+  
+  validity = function(object){
+    if(object@N%%1 != 0 | object@N < 1){
+      return("Error: Number of steps needs to be a natural number")
+    }
+  },
+  
+  contains = c("Option", "VIRTUAL")
+)
+
+setMethod("initialize", "TreeEuropeanOption",
+          function(.Object, ...) {
+            .Object <- callNextMethod(.Object, ...)
+            validObject(.Object)
+            .Object@YearsPerTimesstep <- .Object@Years / .Object@N
+            .Object@DiscountFactorPerTimeStep <- exp(-(.Object@r - .Object@div)*.Object@YearsPerTimesstep)
+            return(.Object)
+          }
+)
+
+BinomialEuropeanOption <- setClass(
+  
+  "BinomialEuropeanOption",
+  
+  slots = c(
+    pu = "numeric", #%change in a up move
+    pd = "numeric" #%change in a down move
+  ),
+  
+  validity = function(object){
+    if(object@pu <= 0 | object@pd <= 0){
+      return("Error: You can't have probabilities smaller or equal to 0")
+    }
+  },
+  
+  contains = "TreeEuropeanOption"
+)
