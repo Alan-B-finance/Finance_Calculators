bbe54c0ff4ad3c0d37dd4de328fac5bf2416a6fd
Alan <37475891+Alan-B-finance@users.noreply.github.com>
2022-11-08 10:01:54 +0100

    Binomial calculator for European Stock Options

--- Option_Calculator.R
+++ Option_Calculator.R
@@ -8,12 +8,13 @@ Option <- setClass(
     r = "numeric", #Risk Free Rate during the live of the Option
     cD = "numeric", #Context Date in yyyymmdd format
     mD = "numeric", #Maturity Date in yyyymmdd format
-    div = "numeric", #dividend yield
+    div = "numeric", #Dividend yield
     flag = "character", #c for call, p for put
     flavor = "character", #e for European, a for American
+    p = "numeric", #Price of the option
     
     Years = "numeric", #time to maturity in years
-    DiscountFactorPerTimeStep = "numeric" #discouning factor used for drift
+    DiscountFactor = "numeric" #Overall discount Factor to maturity
     ),
   
   prototype = list(
@@ -43,12 +44,15 @@ setMethod("initialize", "Option",
           function(.Object, ...) {
             .Object <- callNextMethod(.Object, ...)
             validObject(.Object)
+            
             .Object@Years <- as.numeric(as.Date(as.character(.Object@mD), format("%Y%m%d")) - as.Date(as.character(.Object@cD), format("%Y%m%d")))/365
-            .Object@YearsPerTimesstep <- .Object@Years / .Object@N
-            .Object@DiscountFactorPerTimeStep <- exp(-(.Object@r - .Object@div)*.Object@YearsPerTimesstep)
+            .Object@DiscountFactor <- 1/exp(.Object@r - .Object@div)^(.Object@Years)
+            
             return(.Object)
           }
 )
+
+
 
 TreeEuropeanOption <- setClass(
   
@@ -57,7 +61,7 @@ TreeEuropeanOption <- setClass(
   slots = c(
     N = "numeric", #Number of steps
     
-    YearsPerTimesstep = "numeric" #duration of a single time step,in years
+    YearsPerTimeStep = "numeric" #duration of a single time step,in years
   ),
   
   validity = function(object){
@@ -73,8 +77,9 @@ setMethod("initialize", "TreeEuropeanOption",
           function(.Object, ...) {
             .Object <- callNextMethod(.Object, ...)
             validObject(.Object)
-            .Object@YearsPerTimesstep <- .Object@Years / .Object@N
-            .Object@DiscountFactorPerTimeStep <- exp(-(.Object@r - .Object@div)*.Object@YearsPerTimesstep)
+            
+            .Object@YearsPerTimeStep <- .Object@Years / .Object@N
+            
             return(.Object)
           }
 )
@@ -84,15 +89,52 @@ BinomialEuropeanOption <- setClass(
   "BinomialEuropeanOption",
   
   slots = c(
-    pu = "numeric", #%change in a up move
-    pd = "numeric" #%change in a down move
+    cu = "numeric", #%change in a up move
+    cd = "numeric", #%change in a down move
+    TP = "numeric", #Vector of terminal prices
+    
+    pu = "numeric", #Probability of price changing up, in risk-neutral regime
+    pd = "numeric" #Probability of price changing down, in risk-neutral regime
   ),
   
   validity = function(object){
-    if(object@pu <= 0 | object@pd <= 0){
-      return("Error: You can't have probabilities smaller or equal to 0")
+    if(object@cu <= 0 | object@cd <= 0){
+      return("Error: You can't have changes in prices smaller or equal to 0")
     }
   },
   
   contains = "TreeEuropeanOption"
+)
+
+setMethod("initialize", "BinomialEuropeanOption",
+          function(.Object, ...) {
+            .Object <- callNextMethod(.Object, ...)
+            validObject(.Object)
+            
+            .Object@pu <- (exp((.Object@r - .Object@div)*.Object@YearsPerTimeStep)-(1-.Object@cd))/((1+.Object@cu)-(1-.Object@cd))
+            .Object@pd <- 1-.Object@pu
+            
+            return(.Object)
+          }
+)
+
+setGeneric("BinomialEuropeanOptionStockPrices", function(optionName) 
+  standardGeneric("BinomialEuropeanOptionStockPrices") )
+
+setMethod(f="BinomialEuropeanOptionStockPrices", signature="BinomialEuropeanOption", 
+          definition=function(optionName) {
+            EndNodeRevenue <- c()
+            ProbabilityTable <- c()
+            for(i in 0:optionName@N){
+              EndNodeRevenue[i+1] <- optionName@S0 * ((1+optionName@cu)^(optionName@N-i)) * (1-optionName@cd)^i
+              ProbabilityTable[i+1] <- factorial(optionName@N)/(factorial(i)*factorial(optionName@N-i)) * optionName@pu^(optionName@N-i)*optionName@pd^i
+            }
+            if (optionName@flag == "c") {
+              PayOut <- pmax(0, EndNodeRevenue - optionName@K)
+            } else if (optionName@flag == "p"){
+              PayOut <- pmax(0, optionName@K - EndNodeRevenue)
+            }
+            PayOut <- PayOut * optionName@DiscountFactor
+            return(sum(PayOut * ProbabilityTable))
+        }
 )
