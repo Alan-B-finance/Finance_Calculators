82f6fb7b17e1d25e9f0a0a5b0c79a4d8d199a55f
Alan <37475891+Alan-B-finance@users.noreply.github.com>
2022-11-08 22:42:59 +0100

    American Binomial pricer

--- Option_Calculator.R
+++ Option_Calculator.R
@@ -15,7 +15,7 @@ Option <- setClass(
     
     Years = "numeric", #time to maturity in years
     DiscountFactor = "numeric" #Overall discount Factor to maturity
-    ),
+  ),
   
   prototype = list(
     div = 0
@@ -34,6 +34,10 @@ Option <- setClass(
       return("Error: The flag can either be c (call) or p (put)")
     } else if(!(object@flavor %in% c("e", "a"))){
       return("Error: The flag can either be e (european) or a (american)")
+    } else if(length(object@p > 0)){
+        if(object@p < 0){
+          return("Error: Price of an option can't be lower than 0")
+      }
     }
   },
   
@@ -96,7 +100,7 @@ BinomialEuropeanOption <- setClass(
   
   validity = function(object){
     if(object@cu <= 0 | object@cd <= 0){
-      return("Error: You can't have changes in prices smaller or equal to 0")
+      return("Error: You can't have changes in price smaller or equal to 0")
     }
   },
   
@@ -119,6 +123,8 @@ setGeneric("BinomialEuropeanOptionStockPrices", function(optionName)
   standardGeneric("BinomialEuropeanOptionStockPrices") )
 
 setMethod(f="BinomialEuropeanOptionStockPrices", signature="BinomialEuropeanOption", 
+          #Method does not calculate the entire tree, only the end nodes are needed for European Option
+          
           definition=function(optionName) {
             EndNodeRevenue <- c()
             ProbabilityTable <- dbinom(seq(0, optionName@N, 1), optionName@N, optionName@pd)
@@ -133,7 +139,78 @@ setMethod(f="BinomialEuropeanOptionStockPrices", signature="BinomialEuropeanOpti
             PayOut <- PayOut * optionName@DiscountFactor
             optionName@p <- sum(PayOut * ProbabilityTable)
             nameObject <- deparse(substitute(MyOption))
-            assign(nameObject, optionName, envir=parent.frame())
-            return(sum(PayOut * ProbabilityTable))
-        }
+            assign(nameObject, optionName, envir=parent.frame()) #Overrides a price in the global environment
+            return(optionName@p)
+          }
+)
+
+BinomialAmericanOption <- setClass(
+  
+  "BinomialAmericanOption",
+  
+  slots = c(
+    cu = "numeric", #%change in a up move
+    cd = "numeric", #%change in a down move
+    
+    pu = "numeric", #Probability of price changing up, in risk-neutral regime
+    pd = "numeric", #Probability of price changing down, in risk-neutral regime
+    YearsPerTimesstep = "numeric", #Duration of a single time step,in years
+    DiscountFactorPerTimeStep = "numeric" #Discouning factor used for drift
+  ),
+  
+  validity = function(object){
+    if(object@cu <= 0 | object@cd <= 0){
+      return("Error: You can't have changes in price smaller or equal to 0")
+    }
+  },
+  
+  contains = "TreeEuropeanOption"
+)
+
+setMethod("initialize", "BinomialAmericanOption",
+          function(.Object, ...) {
+            .Object <- callNextMethod(.Object, ...)
+            validObject(.Object)
+            
+            .Object@pu <- (exp((.Object@r - .Object@div)*.Object@YearsPerTimeStep)-(1-.Object@cd))/((1+.Object@cu)-(1-.Object@cd))
+            .Object@pd <- 1-.Object@pu
+            .Object@YearsPerTimesstep <- .Object@Years / .Object@N
+            .Object@DiscountFactorPerTimeStep <- exp(-(.Object@r - .Object@div)*.Object@YearsPerTimesstep)
+            
+            return(.Object)
+          }
+)
+
+setGeneric("BinomialAmericanOptionStockPrices", function(optionName) 
+  standardGeneric("BinomialAmericanOptionStockPrices") )
+
+setMethod(f="BinomialAmericanOptionStockPrices", signature="BinomialAmericanOption", 
+          definition=function(optionName) {
+            StockMovement <- matrix(nrow = optionName@N+1, ncol = optionName@N+1)
+            EndNodeRevenue <- c()
+            for(j in 1:ncol(StockMovement)){
+              for(i in 1:j){
+                StockMovement[i, j] <- optionName@S0 * ((1+optionName@cu)^(j-i)) * (1-optionName@cd)^(i-1)# / optionName@DiscountFactorPerTimeStep^j
+              }
+            }
+            if (optionName@flag == "c") {
+              PayOut <- matrix(pmax(0, StockMovement - optionName@K), nrow = optionName@N+1, ncol = optionName@N+1)
+            } else if (optionName@flag == "p"){
+              PayOut <- matrix(pmax(0, optionName@K - StockMovement), nrow = optionName@N+1, ncol = optionName@N+1)
+            }
+            
+            MidNodeRevenue <- matrix(nrow = optionName@N+1, ncol = optionName@N+1)
+            MidNodeRevenue[, ncol(MidNodeRevenue)] <- PayOut[, ncol(PayOut)]
+            for(i in (ncol(MidNodeRevenue)-1):1){
+              for(k in 1:i){
+                MidNodeRevenue[k, i] <- (MidNodeRevenue[k, i+1] * optionName@pu + MidNodeRevenue[k+1, i+1] * optionName@pd) * optionName@DiscountFactorPerTimeStep
+              }
+              MidNodeRevenue[, i] <- pmax(MidNodeRevenue[, i], PayOut[, i])
+            }
+            
+            optionName@p <- MidNodeRevenue[1, 1]
+            nameObject <- deparse(substitute(MyOption))
+            assign(nameObject, optionName, envir=parent.frame()) #Overrides a price in the global environment
+            return(optionName@p)
+          }
 )
